e7b28fc779f0cc1020b16884c51d58ac215bfbcb
wendy@Wangui <wendyhannah67@gmail.com>
2024-12-08 13:06:59 +0300

    Updated database configuration and prepared for deployment

--- config/config.py
+++ config/config.py
@@ -1,17 +1,26 @@
 import os
+
 from dotenv import load_dotenv
 
-# Load environment variables from the .env file
 load_dotenv()
 
+
 class Config:
+
     # Disable modification tracking overhead
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Database Configuration
-    DATABASE_URL = os.getenv("DATABASE_URL")  # Get DATABASE_URL from .env
+    DB_HOST = os.getenv("POSTGRES_HOST", "database")
+    DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+    DB_NAME = os.getenv("POSTGRES_DB", "uwazi_db")
+    DB_USER = os.getenv("POSTGRES_USER", "uwazi_user")
+    DB_PASSWORD = os.getenv(
+        "POSTGRES_PASSWORD", "uwazi_password"
+    )
 
     @staticmethod
     def get_db_uri():
-        # Return DATABASE_URL directly
-        return Config.DATABASE_URL
+        return (
+            f"postgresql://{Config.DB_USER}:{Config.DB_PASSWORD}@database:{Config.DB_PORT}/{Config.DB_NAME}"
+        )
